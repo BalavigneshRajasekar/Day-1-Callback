ddea334cb7daff706fe82c4d7d6eae450287f8ad
BalavigneshRajasekar <you@example.com>
2024-04-17 15:49:16 +0530

    New feature implemented

--- script.js
+++ script.js
@@ -21,27 +21,20 @@ setTimeout(function(){
                                     setTimeout(function(){
                                         display.classList.add('h2')
                                         display.innerText="Happy Independence day"
+                                        let img=document.createElement('img')
+                                        let parent=document.getElementById('p')
+                                        img.src="Assets/flag1.png"
+                                        parent.appendChild(img)
                                         
                                         
                                     
                                     },1000)
-                                    
-                                
                                 },1000)
-                                
-                            
                             },1000)
-                        
                         },1000)
-                    
                     },1000)
-                
                 },1000)
-            
             },1000)
-        
         },1000)
-    
     },1000)
-
 },1000)
